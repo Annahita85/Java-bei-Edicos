d4a8ce279d73fe9e761ac03507b803f4de374497
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2023-01-18 16:16:29 +0100

    Add files via upload

---
+++ AnnahitaListFormaterTest.java
@@ -0,0 +1,8 @@
+package de.edicos.azubi.aufgabe1;
+
+public class AnnahitaListFormaterTest extends IListFormaterTest {
+    @Override
+    public IListFormater createInstance() {
+        return new AnnahitaListFormater();
+    }
+}
