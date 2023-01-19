472c9c17ca9abc9b33f406401d348c4a7878c8cf
S.Jafari.K <73299967+Annahita85@users.noreply.github.com>
2023-01-19 09:57:07 +0100

    Add files via upload

---
+++ aufgabe4/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Berechnung Kölner Phonetik.
+ * 
+ * <p>Created 2016-04.</p>
+ * @author ckoepp - Cornelius Köpp &lt;cornelius.koepp@edicos.de&gt; 
+ * @version 1.1
+ */
+package de.edicos.azubi.aufgabe4;
